2b4926030035d01cdb22760202ddc519e9db129d
hyeg0121 <hyeg0121@naver.com>
2023-09-01 19:35:33 +0900

    init

---
+++ src/main/java/com/gogit/gogitserver/GogitServerApplication.java
@@ -0,0 +1,13 @@
+package com.gogit.gogitserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GogitServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GogitServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gogit/gogitserver/GogitServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gogit.gogitserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GogitServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
