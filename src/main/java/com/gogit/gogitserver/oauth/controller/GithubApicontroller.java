4676ba76ea0d431ba899772fe816a7c086c1c69d
hyeg0121 <hyeg0121@naver.com>
2023-09-09 09:13:21 +0900

    계정 정보 가져오기

--- src/main/java/com/gogit/gogitserver/oauth/controller/GithubApicontroller.java
+++ src/main/java/com/gogit/gogitserver/oauth/controller/GithubApicontroller.java
@@ -1,16 +1,42 @@
 package com.gogit.gogitserver.oauth.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.servlet.support.RequestContextUtils;
 
 import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.Map;
 import java.util.Properties;
 
 @Controller
 public class GithubApicontroller {
+
+    @GetMapping(value = "/success")
+    public String success(HttpServletRequest request, Model model) throws JsonProcessingException {
+        Map<String, ?> inputFlashMap = RequestContextUtils.getInputFlashMap(request);
+
+        String response = null;
+
+        if(inputFlashMap != null) {
+            response = (String) inputFlashMap.get("result");
+        }
+
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        Map<String, String> result = objectMapper.readValue(response, Map.class);
+
+        model.addAttribute("result", result);
+
+        return "success";
+    }
 
     @GetMapping("/auth/github/callback")
     public String getCode(@RequestParam String code) throws IOException {
@@ -41,7 +67,7 @@ public class GithubApicontroller {
         conn.disconnect();
 
         System.out.println(responseData);
-        return "login-token";
+        return "redirect:/success";
 
     }
 
@@ -56,5 +82,27 @@ public class GithubApicontroller {
             }
         }
         return sb.toString();
+    }
+
+    public void access(String response, RedirectAttributes redirectAttributes) throws IOException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        Map<String, String> map = objectMapper.readValue(response, Map.class);
+        String access_token = map.get("access_token");
+
+        URL url = new URL("https://api.github.com/user");
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setRequestMethod("GET");
+        conn.setRequestProperty("Accept", "application/json");
+        conn.setRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) " +
+                "Chrome/96.0.4664.45 Safari/537.36");
+        conn.setRequestProperty("Authorization", "token " + access_token);
+
+        int responseCode = conn.getResponseCode();
+
+        String result = getResponse(conn, responseCode);
+
+        conn.disconnect();
+
+        redirectAttributes.addFlashAttribute("result", result);
     }
 }
