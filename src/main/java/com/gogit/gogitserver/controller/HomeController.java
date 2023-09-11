78cd561842c2906a0b0524d4704a69a4ec9ef211
hyeg0121 <hyeg0121@naver.com>
2023-09-11 20:51:11 +0900

    HomeController 생성

---
+++ src/main/java/com/gogit/gogitserver/controller/HomeController.java
@@ -0,0 +1,45 @@
+package com.gogit.gogitserver.controller;
+
+import com.gogit.gogitserver.member.dto.MemberDto;
+import jakarta.servlet.http.HttpSession;
+import org.kohsuke.github.GHUser;
+import org.kohsuke.github.GitHub;
+import org.kohsuke.github.GitHubBuilder;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Hello, Home";
+    }
+
+    @GetMapping("/secured")
+    public ResponseEntity secured(@AuthenticationPrincipal OAuth2User oAuth2User,
+                          HttpSession session) throws IOException {
+
+        GitHub gitHub = new GitHubBuilder()
+                .withOAuthToken(session.getAttribute("oAuthToken").toString(),
+                        oAuth2User.getName()).build();
+
+        GHUser user = gitHub.getUser(oAuth2User.getName());
+        System.out.println("user = " + user);
+
+        MemberDto dto = MemberDto.builder()
+                .githubID(user.getLogin())
+                .name(user.getName())
+                .email(user.getEmail())
+                .build();
+
+
+        return ResponseEntity.ok(dto);
+    }
+
+}
