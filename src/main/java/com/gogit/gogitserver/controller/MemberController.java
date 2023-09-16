43a004c0c3a026f23e1c432360f91f10e12265aa
hyeg0121 <hyeg0121@naver.com>
2023-09-16 23:48:10 +0900

    MemberController 생성

---
+++ src/main/java/com/gogit/gogitserver/controller/MemberController.java
@@ -0,0 +1,41 @@
+package com.gogit.gogitserver.controller;
+
+import com.gogit.gogitserver.entity.Member;
+import com.gogit.gogitserver.service.MemberService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/members")
+@AllArgsConstructor
+public class MemberController {
+    private final MemberService memberService;
+
+    @GetMapping
+    public ResponseEntity<List<Member>> getAllMembers() {
+        return ResponseEntity.ok(memberService.getAllMembers());
+    }
+
+    @GetMapping("/{memberId}")
+    public ResponseEntity<Member> getMemberById(@PathVariable Long memberId) {
+        return memberService.getMemberByID(memberId);
+    }
+
+    @PostMapping
+    public ResponseEntity<Member> createMember(@RequestBody Member member) {
+        return memberService.createMember(member);
+    }
+
+    @PutMapping("/{memberId}")
+    public ResponseEntity<Member> updateMember(@PathVariable Long memberId, @RequestBody Member member) {
+        return memberService.updateMember(memberId, updateMember());
+    }
+
+    @DeleteMapping("/{memberId}")
+    public ResponseEntity<Object> deleteMember(@PathVariable Long memberId) {
+        return memberService.deleteMember(memberId);
+    }
+}
