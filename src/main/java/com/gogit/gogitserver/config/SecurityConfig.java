6a2a29b467bd35fb7445c256096af8ec3bcea7ec
hyeg0121 <hyeg0121@naver.com>
2023-10-11 22:48:19 +0900

    토큰 방식 인증을 위해 폼로그인과 세션 비활성화

--- src/main/java/com/gogit/gogitserver/config/SecurityConfig.java
+++ src/main/java/com/gogit/gogitserver/config/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.*;
@@ -16,15 +17,20 @@ public class SecurityConfig {
 
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        return http
-                .authorizeHttpRequests(auth -> {
-                    auth.requestMatchers("/").permitAll();
-                    auth.requestMatchers("/api/members").permitAll();
-                    auth.anyRequest().authenticated();
-                })
-                .oauth2Login(withDefaults())
-                .formLogin(withDefaults())
-                .csrf(AbstractHttpConfigurer::disable)
-                .build();
+
+        http.authorizeHttpRequests(auth -> {
+            auth.requestMatchers("/").permitAll();
+            auth.requestMatchers("/api/members").permitAll();
+            auth.anyRequest().authenticated();
+        })
+        .oauth2Login(withDefaults())
+        .formLogin(withDefaults())
+        .csrf(AbstractHttpConfigurer::disable)
+        .formLogin(AbstractHttpConfigurer::disable)
+        .httpBasic(AbstractHttpConfigurer::disable)
+        .logout(AbstractHttpConfigurer::disable);
+
+
+        return http.build();
     }
 }
