653d50c046e05ac36732a7d7ba3146f9e3ca982a
hyeg0121 <w2218@e-mirim.hs.kr>
2023-10-15 12:06:14 +0900

    오타 수정

--- src/main/java/com/gogit/gogitserver/config/oauth/OAuth2AuthorizationRequestBasedOnCookieRepository.java
+++ src/main/java/com/gogit/gogitserver/config/oauth/OAuth2AuthorizationRequestBasedOnCookieRepository.java
@@ -10,7 +10,7 @@ import org.springframework.web.util.WebUtils;
 
 public class OAuth2AuthorizationRequestBasedOnCookieRepository implements
         AuthorizationRequestRepository<OAuth2AuthorizationRequest> {
-색
+
     public final static String OAUTH2_AUTHORIZATION_REQUEST_COOKIE_NAME = "oauth2_auth_request";
     private final static int COOKIE_EXPIRE_SECONDS = 18000;
 
