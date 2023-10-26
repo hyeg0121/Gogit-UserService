d5ff7e2113e88aa708aed2f0ef01c1ff5d48f557
hyeg0121 <w2218@e-mirim.hs.kr>
2023-10-26 23:33:17 +0900

    ArticleRepository

---
+++ src/main/java/com/gogit/gogitserver/repository/ArticleRepository.java
@@ -0,0 +1,10 @@
+package com.gogit.gogitserver.repository;
+
+import com.gogit.gogitserver.entity.Article;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ArticleRepository extends JpaRepository<Article, Long> {
+
+}
