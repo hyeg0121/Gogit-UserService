a5a7508dfdd56708481b41e0c83a5c8db6952668
hyeg0121 <w2218@e-mirim.hs.kr>
2023-11-15 19:20:25 +0900

    test junit build

--- src/test/java/com/gogit/gogitserver/GogitServerApplicationTests.java
+++ src/test/java/com/gogit/gogitserver/GogitServerApplicationTests.java
@@ -3,7 +3,6 @@ package com.gogit.gogitserver;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class GogitServerApplicationTests {
 
 	@Test
